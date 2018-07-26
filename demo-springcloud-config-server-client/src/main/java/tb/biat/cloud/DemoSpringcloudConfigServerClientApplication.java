08bd2130e0dbf82c8b6937dc83ac6c32bb7785c5
Mohamed Romdhani <mromdhani7@gmail.com>
2018-07-26 08:49:13 +0100

    Commit initial

---
+++ demo-springcloud-config-server-client/src/main/java/tb/biat/cloud/DemoSpringcloudConfigServerClientApplication.java
@@ -0,0 +1,12 @@
+package tb.biat.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringcloudConfigServerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringcloudConfigServerClientApplication.class, args);
+	}
+}
